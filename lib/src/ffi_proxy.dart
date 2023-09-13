5739fba46ef345181d0a15be1f1fe42c8ea08324
vm75 <vm75.dev@gmail.com>
2023-09-13 08:48:11 -0700

    testing new wasm_ffi design

--- lib/src/ffi_proxy.dart
+++ lib/src/ffi_proxy.dart
@@ -1,3 +1,5 @@
 export 'package:wasm_ffi/wasm_ffi.dart' if (dart.library.ffi) 'dart:ffi';
+export 'package:wasm_ffi/wasm_ffi_utils.dart'
+    if (dart.library.ffi) 'package:ffi/ffi.dart';
 export 'wasm_provider.dart' if (dart.library.ffi) 'ffi_provider.dart';
 export 'abstract_platform_provider.dart';
--- lib/src/wasm_provider.dart
+++ lib/src/wasm_provider.dart
@@ -1,80 +1,10 @@
-import 'dart:typed_data';
-
 import 'package:flutter/services.dart';
-import 'package:js/js_util.dart';
-import 'package:wasm_interop/wasm_interop.dart' as interop;
 import 'package:wasm_ffi/wasm_ffi.dart';
-import 'package:wasm_ffi/wasm_ffi_modules.dart';
-import 'package:wasm_ffi/wasm_ffi_meta.dart';
 
 import 'abstract_platform_provider.dart';
 
 typedef UnsignedLong = Uint64;
-typedef Int = Int32;
 typedef Size = Uint64;
-
-/// Documentation is in `emscripten_module_stub.dart`!
-@extra
-class _WasmModule extends Module {
-  final interop.Instance _instance;
-  final List<WasmSymbol> _exports = [];
-
-  @override
-  List<WasmSymbol> get exports => _exports;
-
-  static Future<_WasmModule> initFromAsset(String path) async {
-    final bytes = await rootBundle.load(path);
-    return initFromBuffer(bytes.buffer);
-  }
-
-  static Future<_WasmModule> initFromBuffer(ByteBuffer buffer) async {
-    final wasmInstance =
-        await interop.Instance.fromBytesAsync(buffer.asUint8List());
-    return _WasmModule._(wasmInstance);
-  }
-
-  FunctionDescription _fromWasmFunction(String name, Function func, int index) {
-    String? s = getProperty(func, 'name');
-    if (s != null) {
-      int? length = getProperty(func, 'length');
-      if (length != null) {
-        return FunctionDescription(
-            tableIndex: index,
-            name: name,
-            function: func,
-            argumentCount: length);
-      }
-    }
-    throw ArgumentError('$name does not seem to be a function symbol!');
-  }
-
-  _WasmModule._(this._instance) {
-    int index = 0;
-    for (final e in _instance.functions.entries) {
-      _exports.add(_fromWasmFunction(e.key, e.value, index++));
-    }
-  }
-
-  @override
-  void free(int pointer) {
-    final func = _instance.functions['free'];
-    func?.call(pointer);
-  }
-
-  @override
-  ByteBuffer get heap => _instance.memories['memory']!.buffer;
-
-  @override
-  int malloc(int size) {
-    final func = _instance.functions['malloc'];
-    final resp = func?.call(size) as int;
-    return resp;
-  }
-
-  Function? getMethod(String methodName) {
-    return _instance.functions[methodName];
-  }
-}
 
 class SwephPlatformProvider
     extends AbstractPlatformProvider<DynamicLibrary, Allocator> {
@@ -82,17 +12,15 @@ class SwephPlatformProvider
 
   static Future<SwephPlatformProvider> get instance => _instance;
 
-  final _WasmModule _module;
-
-  SwephPlatformProvider._(
-      this._module, super.lib, super.allocator, super.epheFilesPath);
+  SwephPlatformProvider._(super.lib, super.allocator, super.epheFilesPath);
 
   static Future<SwephPlatformProvider> _init() async {
-    Memory.init();
-    final module =
-        await _WasmModule.initFromAsset('packages/sweph/assets/sweph.wasm');
-    return SwephPlatformProvider._(module, DynamicLibrary.fromModule(module),
-        Memory.global!, "ephe_files");
+    final bytes = await rootBundle.load('packages/sweph/assets/sweph.wasm');
+
+    DynamicLibrary lib = await DynamicLibrary.open(WasmType.standalone,
+        wasmBinary: bytes.buffer.asUint8List());
+
+    return SwephPlatformProvider._(lib, lib.boundMemory, "ephe_files");
   }
 
   @override
@@ -102,16 +30,16 @@ class SwephPlatformProvider
     final destPathPtr = _copyToWasm(Uint8List.fromList(destPath.codeUnits));
     final dataPtr = _copyToWasm(contents);
 
-    final writeFile = _module.getMethod('write_file')!;
+    final writeFile = lib.lookupFunction('write_file');
     writeFile.call(destPathPtr, dataPtr, contents.length, 0);
 
-    _module.free(destPathPtr);
+    lib.module.free(destPathPtr);
   }
 
   int _copyToWasm(Uint8List data) {
     final size = data.length;
-    final dataPtr = _module.malloc(size + 1);
-    final memoryView = _module.heap.asUint8List();
+    final dataPtr = lib.module.malloc(size + 1);
+    final memoryView = lib.module.heap.asUint8List();
     memoryView.setAll(dataPtr, data);
     memoryView[dataPtr + size] = 0;
     return dataPtr;
